b2c2dff4aa7d0f71ff9da64945f910f749f10d94
LuisAngel <luisangel.salvat@gmail.com>
2021-02-11 18:57:56 -0500

    Initial commit

---
+++ app/src/main/java/com/qbo/appreceptorintentkea3/ReceptorActivity.kt
@@ -0,0 +1,11 @@
+package com.qbo.appreceptorintentkea3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceptorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receptor)
+    }
+}
